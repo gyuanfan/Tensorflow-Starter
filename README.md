3c9cb8931161c50893d84b7ff7fc32fd257f9962
gyuanfan <yuanfang.guan@gmail.com>
2017-12-24 17:19:33 -0500

    alex net example

---
+++ README.md
@@ -0,0 +1,4 @@
+Starter code for TensorFlow
+
+1. resnet ## train resnet from scratch for a 2-channel data
+2. alexnet ## train alexnet from scratch for a 2-channel data
---
+++ alexnet/bash.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+python train.py list_train 224
+
+python predict_all_vis.py fold0
+mv pred.txt pred.fold0
+
+python predict_all_vis.py fold1
+mv pred.txt pred.fold1
+
+python predict_all_vis.py fold2
+mv pred.txt pred.fold2
+
+python predict_all_vis.py fold3
+mv pred.txt pred.fold3
+
+python predict_all_vis.py fold4
+mv pred.txt pred.fold4
